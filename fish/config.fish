ee5b16635ea01ab9580ca748577fcf59b2c26f75
GervinFung <gervinfungdaxuen@gmail.com>
2022-09-29 23:27:09 +0800

    fix(lua-config): invalid lua config warning

--- fish/config.fish
+++ fish/config.fish
@@ -37,6 +37,14 @@ set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
 
+# Android
+# Refer to https://gist.github.com/ivopr/3f2ee281cd38e66dc1a4d1344154b715
+#set -Ux ANDROID_HOME $HOME/Android/Sdk
+#set -U fish_user_paths $ANDROID_HOME/emulator $fish_user_paths
+#set -U fish_user_paths $ANDROID_HOME/tools $fish_user_paths
+#set -U fish_user_paths $ANDROID_HOME/tools-bin $fish_user_paths
+#set -U fish_user_paths $ANDROID_HOME/platform-tools $fish_user_paths
+
 # This is needed to solve Powerline not being rendered inside tmux
 # Refer https://github.com/gpakosz/.tmux/issues/171#issuecomment-426048355
 set -gx LC_ALL en_US.UTF-8
